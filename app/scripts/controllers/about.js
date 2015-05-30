f9fb9d79ab43fedaa6bdc070301911df80e486d4
juanpanore <juanpanore@gmail.com>
2015-05-30 11:59:19 -0500

    Car sharing first commit with structure project web

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appCarSharingApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appCarSharingApp
+ */
+angular.module('appCarSharingApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appCarSharingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appCarSharingApp
+ */
+angular.module('appCarSharingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
