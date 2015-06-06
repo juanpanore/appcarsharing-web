0458a06c1d312e55b7cbcedd20ac4aa38417ccfc
Tests <anderssongarciasotelo@gmail.com>
2015-06-06 10:10:37 -0500

    Adicionando consumo de servicios web

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var AVAILABLE_BRANDS = 'http://appcarsharing-api.heroku.com/rest/brand';
+
+var CARS_BY_USER = 'http://appcarsharing-api.heroku.com/rest/user/cars';
+
+var USER = 'http://appcarsharing-api.heroku.com/rest/user';
+
 /**
  * @ngdoc function
  * @name appCarSharingApp.controller:MainCtrl
@@ -7,11 +13,49 @@
  * # MainCtrl
  * Controller of the appCarSharingApp
  */
-angular.module('appCarSharingApp')
-  .controller('MainCtrl', function ($scope) {
+var appCarSharingAppModule = angular.module('appCarSharingApp');
+
+appCarSharingAppModule.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
   });
+
+appCarSharingAppModule.service('availableBrandsWebService', function($http) {
+	this.findAllAvailableBrands = function() {
+
+		return ($http({
+			method : 'GET',
+			url : AVAILABLE_BRANDS
+		}));
+	};
+});
+
+appCarSharingAppModule.service('carsByUserWebService', function($http) {
+	this.findAllAvailableBrands = function($cookies) {
+
+		return ($http({
+			method : 'GET',
+			url : CARS_BY_USER,
+			params : {
+				email : $cookies.email;
+			}
+		}));
+	};
+});
+
+appCarSharingAppModule.service('addCarWebService', function($http) {
+	this.findAllAvailableBrand = function(carJSON, $cookies) {
+
+		return ($http({
+			method : 'POST',
+			url : CARS,
+			params : {
+				email : $cookies.email;
+			},
+			data : carJSON
+		}));
+	};
+});
