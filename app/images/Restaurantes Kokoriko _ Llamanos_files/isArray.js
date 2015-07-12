01a30375b34ca8eda939383b384dedd3718a9aa8
juanpanore <juanpanore@gmail.com>
2015-07-12 12:23:30 -0500

    delete no use files

--- app/images/Restaurantes Kokoriko _ Llamanos_files/isArray.js
+++
@@ -1,7 +0,0 @@
-/* Checks if a test element is an array */
-define(function() {
-	function isArray(test) {
-		return Object.prototype.toString.call( test ) === '[object Array]' 
-	}
-	return isArray
-})
